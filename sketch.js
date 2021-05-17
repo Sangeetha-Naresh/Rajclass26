2773eeccb3f3da865be33dc1bd3fd831602a930a
Sangeetha-Naresh <sangeetha.nareshm@gmail.com>
2021-05-17 09:55:47 +0530

    Added orange color to ground class

--- sketch.js
+++ sketch.js
@@ -41,10 +41,9 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+   
     box1.display();
     box2.display();
     ground.display();
@@ -61,5 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+   
     platform.display();
 }
